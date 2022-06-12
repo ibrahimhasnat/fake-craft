98c2797392630ac8162146c42ce98f0ce7f45433
ibrahimhasnat <ibrahimhasnat.id@gmail.com>
2022-06-12 15:16:17 +0600

    Initial Release

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screen: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    colors: {
+      transparent: 'transparent',
+      primaryColor: '#00acee',
+      white: '#FFF',
+      grey: '#f1f5f9',
+      slate: 'rgb(100 116 139)',
+      black: '#000'
+    },   
+    extend: {},
+  },
+  plugins: [],
+}
